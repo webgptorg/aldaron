ae81d1f1eea78eb21489e739d7eb39551c6d3d2b
Pavol Hejny <me@pavolhejny.com>
2025-09-09 13:28:14 +0200

    Fixing build

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log('process.env', process.env);
+console.log('process.env.NEXT_PUBLIC_SUPABASE_URL', process.env.NEXT_PUBLIC_SUPABASE_URL);
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
