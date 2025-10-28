2ff95bdb3d89945811b0d1d9d455705dbc659430
Pavol Hejny <me@pavolhejny.com>
2025-10-28 10:18:42 +0100

    Better team

--- components/team-section.tsx
+++ components/team-section.tsx
@@ -10,61 +10,41 @@ export const TeamSection = () => {
                     </h2>
                 </div>
 
-                <div className="grid grid-cols-1 mt-12 sm:grid-cols-2 lg:grid-cols-2 gap-x-8 gap-y-12 lg:mt-16">
-                    <div className="text-center">
-                        <div className="relative inline-flex">
-                            <div className="absolute -inset-2">
-                                <div
-                                    className="w-full h-full mx-auto rotate-180 opacity-30 blur-lg filter"
-                                    style={{
-                                        background:
-                                            'linear-gradient(90deg, #44ff9a -0.55%, #44b0ff 22.86%, #8b44ff 48.36%, #ff6644 73.33%, #ebff70 99.34%)',
-                                    }}
-                                ></div>
-                            </div>
-                            <Image
-                                className="relative object-cover w-48 h-48 mx-auto rounded-full"
-                                src="https://github.com/webgptorg/promptbook/blob/main/design/people/jiri-jahn-transparent.png?raw=true"
-                                alt="Jiri Jahn"
-                                width={192}
-                                height={192}
-                            />
-                        </div>
-                        <div className="mt-6">
-                            <p className="text-xl font-bold text-gray-900 font-pj">Jiří Jahn</p>
-                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">CEO</p>
+                <div className="grid grid-cols-1 mt-12 sm:grid-cols-2 lg:grid-cols-2 gap-x-8 gap-y-12 lg:mt-16 items-center">
+                    <div className="grid grid-cols-2 gap-x-4 items-center">
+                        <Image
+                            className="object-cover mx-auto"
+                            src="https://github.com/webgptorg/promptbook/blob/main/design/people/jiri-jahn-transparent.png?raw=true"
+                            alt="Jiri Jahn"
+                            width={300}
+                            height={300}
+                        />
+                        <div className="text-left">
+                            <p className="text-xl font-bold text-gray-900 font-pj">CEO | Jiří Jahn</p>
                             <p className="mt-4 text-base text-gray-500">
                                 Ph.D. in Mathematics, former researcher at IT4I National Supercomputing Centre.
                             </p>
+                            <p className="mt-4 text-base text-gray-500">jiri@ptbk.io</p>
+                            <p className="mt-1 text-base text-gray-500">+420 777 090 067</p>
                         </div>
                     </div>
 
-                    <div className="text-center">
-                        <div className="relative inline-flex">
-                            <div className="absolute -inset-2">
-                                <div
-                                    className="w-full h-full mx-auto rotate-180 opacity-30 blur-lg filter"
-                                    style={{
-                                        background:
-                                            'linear-gradient(90deg, #44ff9a -0.55%, #44b0ff 22.86%, #8b44ff 48.36%, #ff6644 73.33%, #ebff70 99.34%)',
-                                    }}
-                                ></div>
-                            </div>
-                            <Image
-                                className="relative object-cover w-48 h-48 mx-auto rounded-full"
-                                src="https://github.com/webgptorg/promptbook/blob/main/design/people/pavol-hejny-transparent.png?raw=true"
-                                alt="Pavol Hejný"
-                                width={192}
-                                height={192}
-                            />
-                        </div>
-                        <div className="mt-6">
-                            <p className="text-xl font-bold text-gray-900 font-pj">Pavol Hejný</p>
-                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">CTO</p>
+                    <div className="grid grid-cols-2 gap-x-4 items-center">
+                        <div className="text-left">
+                            <p className="text-xl font-bold text-gray-900 font-pj">Pavol Hejný | CTO</p>
                             <p className="mt-4 text-base text-gray-500">
                                 Top open-source contributor in CZE. Developer with 15+ years of experience.
                             </p>
+                            <p className="mt-4 text-base text-gray-500">pavol@ptbk.io</p>
+                            <p className="mt-1 text-base text-gray-500">+420 777 759 767</p>
                         </div>
+                        <Image
+                            className="object-cover mx-auto"
+                            src="https://github.com/webgptorg/promptbook/blob/main/design/people/pavol-hejny-transparent.png?raw=true"
+                            alt="Pavol Hejný"
+                            width={300}
+                            height={300}
+                        />
                     </div>
                 </div>
             </div>
