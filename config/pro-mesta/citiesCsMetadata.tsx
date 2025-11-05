8a9f9a78c4eed9ca71326edd0100100d8e270ff3
Pavol Hejny <me@pavolhejny.com>
2025-11-05 02:32:40 +0100

    Add `openGraph` to `citiesCsMetadata`

--- config/pro-mesta/citiesCsMetadata.tsx
+++ config/pro-mesta/citiesCsMetadata.tsx
@@ -1,3 +1,4 @@
+import ogImage from '@public/logo/og-image.png';
 import { Metadata } from 'next';
 
 export const citiesCsMetadata: Metadata = {
@@ -12,6 +13,14 @@ export const citiesCsMetadata: Metadata = {
         description:
             'With Promptbook, you can capture the context, rules, and knowledge of your organization into simple documents and create AI agents that perfectly match your needs.',
         url: 'https://ptbk.io/pro-mesta',
+        images: [
+            {
+                url: ogImage.src,
+                width: 1860,
+                height: 992,
+                alt: 'AI Transformation for Cities and Municipalities - Promptbook',
+            },
+        ],
     },
     twitter: {
         title: 'AI Transformation for Cities and Municipalities - Promptbook',
