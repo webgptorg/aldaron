43b49c712a9058fb6c897439cc72a15d3cc4e9ca
Pavol Hejny <me@pavolhejny.com>
2026-04-30 15:27:22 +0200

    Add meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { GOOGLE_ANALYTICS_ID } from '@/config';
 import { Metadata } from 'next';
 import { Inter, Outfit } from 'next/font/google';
 import Script from 'next/script';
+import spaceTrim from 'spacetrim';
 import { Chatbot } from '../components/chatbot';
 import { CookiesBar } from '../components/cookies-bar';
 import './globals.css';
@@ -86,6 +87,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <meta name="theme-color" content="#79EAFD" />
                 <meta name="msapplication-TileColor" content="#79EAFD" />
                 <meta name="msapplication-config" content="/browserconfig.xml" />
+
                 {/* Google Analytics */}
                 <Script
                     async
@@ -93,16 +95,46 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     strategy="afterInteractive"
                 />
                 <Script id="google-analytics" strategy="afterInteractive">
-                    {`
+                    {spaceTrim(`
+                        console.log('📊 Google Analytics initialized');
+
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
                         gtag('config', '${GOOGLE_ANALYTICS_ID}');
-                    `}
+                    `)}
+                </Script>
+
+                {/* Meta Pixel Code */}
+                <Script id="meta-pixel" strategy="afterInteractive">
+                    {spaceTrim(`
+                        console.log('📈 Meta Pixel initialized');
+
+                        !function(f,b,e,v,n,t,s)
+                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                        n.queue=[];t=b.createElement(e);t.async=!0;
+                        t.src=v;s=b.getElementsByTagName(e)[0];
+                        s.parentNode.insertBefore(t,s)}(window, document,'script',
+                        'https://connect.facebook.net/en_US/fbevents.js');
+                        fbq('init', '1211494690544539');
+                        fbq('track', 'PageView');
+                    `)}
                 </Script>
             </head>
             <body className={`${inter.variable} ${outfit.variable} font-sans`}>
                 {children}
+                {/* Meta Pixel NoScript */}
+                <noscript>
+                    <img
+                        height="1"
+                        width="1"
+                        style={{ display: 'none' }}
+                        src="https://www.facebook.com/tr?id=1211494690544539&ev=PageView&noscript=1"
+                        alt="Meta Pixel"
+                    />
+                </noscript>
                 <Chatbot />
                 <CookiesBar />
             </body>
