7535a046eb4443eef36e15a3a7e2d25d2f118fcd
Pavol Hejny <me@pavolhejny.com>
2026-05-02 09:53:45 +0200

    Rename app name

--- config.ts
+++ config.ts
@@ -1,11 +1,11 @@
 /**
  * Name of the application
  */
-export const APP_NAME = 'New landing page';
+export const APP_NAME = 'Landing page';
 
 /**
  * ID of the Google Analytics
  *
- * @see https://analytics.google.com/analytics/web/?utm_source=marketingplatform.google.com&utm_medium=et&utm_campaign=marketingplatform.google.com%2Fabout%2Fanalytics%2F#/a344862085p495760868/realtime/overview?params=_u..nav%3Dmaui&collectionId=business-objectives
+ * @see https://analytics.google.com/analytics/web/#/a344862085p495760868/reports/intelligenthome?params=_u..nav%3Dmaui
  */
 export const GOOGLE_ANALYTICS_ID = 'G-LDZBXVZHT0';
